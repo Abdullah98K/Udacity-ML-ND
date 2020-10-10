e79bb60d1ec5090f870271aaedf6d9e5e8b1b8dc
Abdullah <60227491+Abdullah98K@users.noreply.github.com>
2020-10-10 23:00:20 +0300

    Delete util.py

--- Projects/charity_ml/util.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-def process_image(image):
-    image = tf.convert_to_tensor(image)
-    image = tf.image.resize(images=image,size=[224,224])
-    image = tf.cast(image, tf.float32)
-    image /= 255
-    return image.numpy()
